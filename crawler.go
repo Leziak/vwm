0f38f13255c040d0bb06ec4a1353a768de9860f2
majorsam <majorsam@fit.cvut.cz>
2020-04-19 23:27:58 +0200

    Changed crawler to get a few thousands links and stop

--- crawler.go
+++ crawler.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	// "database/sql"
+	"net/url"
 	"fmt"
 	"github.com/gocolly/colly"
 	_ "github.com/lib/pq"
 	"os"
 	"strings"
-	"sync"
+	"math/rand"
+	"time"
 )
 
 // const (
@@ -33,31 +35,51 @@ import (
 // 	}
 // }
 
-type safeURL struct {
-	url string
-	mux sync.Mutex
+func getRand(min int, max int) int {
+		rand.Seed(time.Now().UnixNano())
+		return rand.Intn(max - min) + min
 }
 
 func crawl(file *os.File) {
-	c := colly.NewCollector(colly.AllowedDomains("en.wikipedia.org"), colly.MaxDepth(2), colly.Async(true))
+	c := colly.NewCollector(colly.AllowedDomains("en.wikipedia.org"), colly.MaxDepth(3), colly.Async(true))
 	outlink := ""
-	url := "https://en.wikipedia.org/wiki/Slovakia"
+	input := "https://en.wikipedia.org/wiki/Slovakia"
 
 	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 2})
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	cnt := 0
+	c.OnHTML("#bodyContent a[href]", func(e *colly.HTMLElement) {
 		inlink := e.Attr("href")
 		outlink = e.Request.URL.String()[24:]
+		cnt++
 
-		if strings.HasPrefix(inlink, "/wiki/") && strings.HasPrefix(outlink, "/wiki/") && inlink != outlink {
-			fmt.Println(outlink, inlink)
-			file.WriteString(outlink + "\t" + inlink + "\n")
+	 /* Tento big brain hack znizi pocet moznych hran v grafe na max zopar tisic tym, 
+		* ze budem robit request iba raz za 50-100 moznych requestov.
+		* Navyse sa bude vystupny subor vzdy lisit od toho predchadzajuceho.
+		*/
+		if n := getRand(50, 100); cnt % n != 0 {
+			return
 		}
 
-		e.Request.Visit(inlink)
+		if strings.HasPrefix(inlink, "/wiki/") && 
+		!strings.HasPrefix(inlink, "/wiki/Wikipedia") &&
+		!strings.HasPrefix(inlink, "/wiki/Category") &&
+		!strings.HasPrefix(inlink, "/wiki/File") &&
+		!strings.HasPrefix(inlink, "/wiki/Help") &&
+		!strings.HasPrefix(inlink, "/wiki/User") &&
+		!strings.HasPrefix(inlink, "/wiki/Template") &&
+		!strings.HasPrefix(inlink, "/wiki/ISBN") &&
+		!strings.HasPrefix(inlink, "/wiki/Portal") && 
+		!strings.HasPrefix(inlink, "/wiki/Special") {
+			decodedOutlink, _ := url.QueryUnescape(outlink)
+			decodedInlink, _ := url.QueryUnescape(inlink)
+			fmt.Println(cnt, outlink, inlink)
+			file.WriteString(decodedOutlink[6:] + "\t" + decodedInlink[6:] + "\n")
+			e.Request.Visit(inlink)
+		}
 	})
 
-	c.Visit(url)
+	c.Visit(input)
 
 	c.Wait()
 }
